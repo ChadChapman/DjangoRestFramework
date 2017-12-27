9a439e2abff99df4ed613976c05a5746b4e97fc7
ChadTC <codecrab.me@gmail.com>
2017-12-27 12:22:46 -0800

    snippets modifying now has permissions

--- drftutorial/drftutorial/urls.py
+++ drftutorial/drftutorial/urls.py
@@ -19,4 +19,6 @@ from django.contrib import admin
 urlpatterns = [
     #url(r'^admin/', admin.site.urls), #leaving out unless causes a bug
     url(r'^', include('snippets.urls')),
+    url(r'^api-auth/', include('rest_framework.urls')), #correct urls.py file?
+    #^^ api-auth part can be changed to whatever
 ]
